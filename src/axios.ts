26149009dd8428d5aa6505812bd8722610e2f9d5
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2025-09-03 02:03:13 +0600

    fix axios baseURL for Vercel deployment

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosApi = axios.create({
-  baseURL: "/api/webapp/api",
+  baseURL: import.meta.env.PROD
+    ? "https://noxer-test.ru/webapp/api"
+    : "/api/webapp/api",
 });
 
 export default axiosApi;
